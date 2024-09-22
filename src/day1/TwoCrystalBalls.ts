3bf356a0b1d2caa733150dff14f4c2a34aa1a7c4
Danil Sergeev <danil.kzn2003@gmail.com>
2024-09-22 18:09:00 +0200

    code two_crystals

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let step = Math.floor(Math.sqrt(breaks.length));
 
+    let index = step;
+    // while (!breaks[index]) {
+    //     index += step;
+    // }
+    for (; index < breaks.length; index += step) {
+        if (breaks[index]) {
+            break;
+        }
+    }
+    index -= step;
+
+    for (let j = 0; j < step && index < breaks.length; ++index, ++j) {
+        if (breaks[index]) {
+            return index;
+        }
+    }
+    // while (index != breaks.length && !breaks[index]) {
+    //     index++;
+    // }
+    return -1;
 }
