0a232fa1772cab2b9a89a9bec8669d5eb0e27e91
Danil Sergeev <danil.kzn2003@gmail.com>
2024-10-10 10:23:08 +0200

    solution to two_crystals

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,24 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-    let step = Math.floor(Math.sqrt(breaks.length));
+    const step = Math.floor(Math.sqrt(breaks.length));
 
-    let index = step;
-    // while (!breaks[index]) {
-    //     index += step;
-    // }
-    for (; index < breaks.length; index += step) {
-        if (breaks[index]) {
-            break;
+    for (let i = step; i < breaks.length; i += step) {
+        if (breaks[i]) {
+            i -= step;
+            for (let j = i; j < i + step; j++) {
+                if (breaks[j]) {
+                    return j;
+                }
+            }
         }
     }
-    index -= step;
-
-    for (let j = 0; j < step && index < breaks.length; ++index, ++j) {
-        if (breaks[index]) {
-            return index;
-        }
-    }
-    // while (index != breaks.length && !breaks[index]) {
-    //     index++;
-    // }
     return -1;
 }
